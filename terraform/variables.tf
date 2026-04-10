e456060f9e522efcef29e77a79bb387fa78b72b7
Ibrahim Cakir <firtinagtrnn@gmail.com>
2026-04-10 22:53:12 +0300

    project files updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,6 +30,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of the existing EC2 key pair for SSH access"
   type        = string
+  default     = "cpt"
 }
 
 # ============================================================
